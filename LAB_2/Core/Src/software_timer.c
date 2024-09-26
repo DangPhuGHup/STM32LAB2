20f9a3c683e3606a28c1721a162177d83bdde84a
Dang Phu <dangquocphupy26102004@gmail.com>
2024-09-26 16:32:03 +0700

    Init EX2 final 1631 26/09/2024

--- LAB_2/Core/Src/software_timer.c
+++ LAB_2/Core/Src/software_timer.c
@@ -9,11 +9,19 @@
 int timer1_counter = 0;
 int timer2_counter = 0;
 int timer3_counter = 0;
+int timer4_counter = 0;
+int timer5_counter = 0;
+int timer6_counter = 0;
+int timerDOT_counter = 0;
 //int timer_SEG1_counter = 0;
 //int timer_SEG2_counter = 0;
 int timer1_flag = 0;
-int timer2_flag = 1;
+int timer2_flag = 0;
 int timer3_flag = 0;
+int timer4_flag = 0;
+int timer5_flag = 1;
+int timer6_flag = 0;
+int timerDOT_flag = 1;
 //int timer_SEG1_flag = 0;
 //int timer_SEG2_flag = 0;
 
@@ -32,6 +40,26 @@ void setTimer3(int duration)
 {
 	timer3_counter = duration;
 	timer3_flag = 0;
+}
+void setTimer4(int duration)
+{
+	timer4_counter = duration;
+	timer4_flag = 0;
+}
+void setTimer5(int duration)
+{
+	timer5_counter = duration;
+	timer5_flag = 0;
+}
+void setTimer6(int duration)
+{
+	timer6_counter = duration;
+	timer6_flag = 0;
+}
+void setTimerDOT(int duration)
+{
+	timerDOT_counter = duration;
+	timerDOT_flag = 0;
 }
 //void setTimer_SEG1(int duration)
 //{
@@ -62,6 +90,39 @@ void timerRun()
 		if(timer3_counter <= 0)
 		{
 			timer3_flag = 1;
+		}
+	}
+
+	if(timer4_counter > 0)
+	{
+		timer4_counter--;
+		if(timer4_counter <= 0)
+		{
+			timer4_flag = 1;
+		}
+	}
+	if(timer5_counter > 0)
+    {
+	    timer5_counter--;
+		if(timer5_counter <= 0)
+		{
+			timer5_flag = 1;
+		}
+	}
+	if(timer6_counter > 0)
+	{
+		timer6_counter--;
+		if(timer6_counter <= 0)
+		{
+			timer6_flag = 1;
+		}
+	}
+	if(timerDOT_counter > 0)
+	{
+		timerDOT_counter--;
+		if(timerDOT_counter <= 0)
+		{
+			timerDOT_flag = 1;
 		}
 	}
 }
